ffb0a211a550e63f702ed9024716420b08bf0705
jackoregankenny <42699197+jackoregankenny@users.noreply.github.com>
2021-08-24 01:24:31 +0100

    Testing Setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            _Oreganic<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">Research</span>
+          <h2>Coming 2022</h2>
           <SocialList />
         </div>
       </div>
